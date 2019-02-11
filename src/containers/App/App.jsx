b0d240026cfa03c55c2721f40f8375156aabde5c
Eric <ericbdev@gmail.com>
2019-02-10 22:08:25 -0500

    App, and Style fix ups
    - Clean up orphaned styles
    - Move over to css modules
    - Correct file extension

---
+++ src/containers/App/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import styles from './App.module.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <main className={styles.container}>
+      </main>
+    );
+  }
+}
+
+export default App;
