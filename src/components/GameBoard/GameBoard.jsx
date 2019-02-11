d95468d74fd33ec56b7e13ba708a1ea2580b94fe
Eric <ericbdev@gmail.com>
2019-02-10 22:43:04 -0500

    Test pass: Populate game board

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,15 +1,28 @@
-import React, { Component }  from 'react';
-
-import styles from './GameBoard.module.scss'
+import React, { Component } from 'react';
+import Proptypes from 'prop-types';
+import Square from '../Square/Square'
+import styles from './GameBoard.module.scss';
 
 class GameBoard extends Component {
+  handleClick = () => {
+
+  }
+
   render() {
-    const classList = `game-board ${styles.container}`
+    const classList = `game-board ${styles.container}`;
+
     return (
       <div className={classList}>
+        {this.props.squares.map((value, index) => {
+          return <Square onClick={this.handleClick} key={index} />;
+        })}
       </div>
-    )
+    );
   }
 }
+
+GameBoard.propTypes = {
+  squares: Proptypes.array.isRequired,
+};
 
 export default GameBoard;
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import styles from './App.module.scss';
 
-import GameBoard from '../../components/GameBoard/GameBoard'
+import GameBoard from '../../components/GameBoard/GameBoard';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.squares = Array(9).fill(null);
   }
 
   render() {
     return (
       <main className={styles.container}>
-        <GameBoard />
+        <GameBoard squares={this.squares} />
       </main>
     );
   }
